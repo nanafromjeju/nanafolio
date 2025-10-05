2cb8c3fde061cb37e28bef1e5eb3101a217b05a8
nana <nanafromjeju@gmail.com>
2025-10-06 03:04:04 +0900

    chore: 프로젝트 기본 세팅

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Main from './pages/Main';
+import './styles/app.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
